be03c9d5e9d67aa02a84e4c5102559cd4b01c2ed
meta-jignesh-mistry <jignesh.mistry@metacube.com>
2015-09-02 13:23:46 +0530

    Update QueueUsingLinkList.java

--- DS/DS-5/queue/QueueUsingLinkList.java
+++ DS/DS-5/queue/QueueUsingLinkList.java
@@ -30,4 +30,27 @@ public  class QueueUsingLinkList extends LinkedList<String>  {
     public String get(int index){
        return linkList.get(index);
    }
+    //method to peek front element
+   public String peek(){
+	   return linkList.get(0);
+   }
+   //method to put element at perticular position
+   public boolean offer(int index,String item){
+	   index--;
+	if(index<linkList.size()&&index>=0){
+		linkList.set(index,item);
+		return true;
+		
+	}
+	else{
+		return false;
+	}
+	   
+	   
+   }
+    
+    
+    
+    
+    
  }
