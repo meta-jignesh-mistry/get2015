9eae418415c6bb63bca0b9c01a62583393f42d6a
meta-jignesh-mistry <jignesh.mistry@metacube.com>
2015-09-02 13:21:58 +0530

    Update QueueDemo.java

--- DS/DS-5/queue/QueueDemo.java
+++ DS/DS-5/queue/QueueDemo.java
@@ -10,13 +10,15 @@ public class QueueDemo {
     
     public static void main(String args[]){
         
-        
+    	String regex="[0-9]+";
         QueueUsingLinkList queue=new QueueUsingLinkList();
         while(true){
         System.out.println("\n\nPress 1 to add Element in queue");
         System.out.println("Press 2 to remove Element from queue");
-        System.out.println("Press 3 to show queue");
-        System.out.println("Press 4 to exit");
+        System.out.println("Press 3 to peek Element from queue");
+        System.out.println("Press 4 to change element at perticular position in queue");
+        System.out.println("Press 5 to show queue");
+        System.out.println("Press 6 to exit");
         Scanner sc=new Scanner(System.in);
         String choice=sc.nextLine();
          
@@ -39,8 +41,40 @@ public class QueueDemo {
             			System.out.println("Front item is removed");
             		}
             			break;
+            			
+            case "3":
+            		if(queue.size()==0){
+            			System.out.println("\nQueue is Empty");
+        	
+            		}else
+            		{	//to peek front element of queue
+            			System.out.println("\nFront element in queue is "+queue.peek());
+            		}
+        				break;
+            			
+            	case "4":
+            				System.out.println("Enter element ");
+            				String item=sc.next();
+            				String positionValue;
+            				do{
+            					System.out.println("Enter position where you want to replace");
+                				positionValue=sc.next();
+                				
+        						
+        					}while(!(positionValue.matches(regex)));
+            				
+            				int position=Integer.parseInt(positionValue);
+            				
+            				if(queue.offer(position, item))
+            				{
+            					System.out.println("item changed successfully");
+            				}
+            				else{
+            					System.out.println("you can't insert item at this position");
+            				}
+        					break;			
                     
-            case "3" :if(queue.size()==0){
+            case "5" :if(queue.size()==0){
     					System.out.println("\nQueue is Empty");
             	
     				}
@@ -58,7 +92,7 @@ public class QueueDemo {
     				}
                 
             			break;
-            case "4": 
+            case "6": 
             		System.exit(0);
             default:
                 	System.out.println("Please enter correct choice");
