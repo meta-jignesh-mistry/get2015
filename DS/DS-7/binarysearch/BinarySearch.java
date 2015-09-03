5f867f3148b2bc9150b02ee5d0d4aeecb6ee9ccc
meta-jignesh-mistry <jignesh.mistry@metacube.com>
2015-09-03 12:49:37 +0530

    Update BinarySearch.java

--- DS/DS-7/binarysearch/BinarySearch.java
+++ DS/DS-7/binarysearch/BinarySearch.java
@@ -11,7 +11,7 @@ public class BinarySearch {
 	}
 	
 	//method will return index value of left most element 
-	public int findLeftMostOccurance(int[] array,int size, int low, int high, int searchElement) {
+	public int findLeftMostOccurrence(int[] array,int size, int low, int high, int searchElement) {
 		
 			if(high<0 || low >size-1){
 				return -1;
@@ -24,19 +24,19 @@ public class BinarySearch {
 				if(searchElement<array[mid])
 				{
 					high=mid-1;
-					return findLeftMostOccurance(array, size,low,high,searchElement);
+					return findLeftMostOccurrence(array, size,low,high,searchElement);
 				}//if mid element is greater than search element
 				else if(searchElement>array[mid])
 				{
 					low=mid+1;
-					return findLeftMostOccurance(array, size,low,high,searchElement);
+					return findLeftMostOccurrence(array, size,low,high,searchElement);
 				}
 				
 				else 
 				{
 					leftMostIndex=mid;
 					//again checking from low to leftMostIndex-1 to find left most index of element
-					findLeftMostOccurance(array, size, low, mid-1, searchElement);
+					findLeftMostOccurrence(array, size, low, mid-1, searchElement);
 					return leftMostIndex;
 				} 
 				
