f791590c9aa3340635e5b498d1b26a8cb4363024
meta-jignesh-mistry <jignesh.mistry@metacube.com>
2015-09-03 16:33:01 +0530

    Update NameComparator.java

--- DS/employee/NameComparator.java
+++ DS/employee/NameComparator.java
@@ -9,7 +9,7 @@ public class NameComparator implements Comparator {
 	public int compare(Object arg1, Object arg2) {
 		Employee emp1 = (Employee) arg1;
 		Employee emp2 = (Employee) arg2;
-		return emp1.getName().compareTo(emp2.getName());
+		return emp1.getName().compareToIgnoreCase(emp2.getName());
 	}
 
 	
