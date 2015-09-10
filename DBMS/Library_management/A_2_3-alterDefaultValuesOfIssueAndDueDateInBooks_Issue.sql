68ad3a1cffc74d2af77a9fa2f7f537e644cb385c
meta-jignesh-mistry <jignesh.mistry@metacube.com>
2015-09-10 11:06:28 +0530

    Update A_2_3-alterDefaultValuesOfIssueAndDueDateInBooks_Issue.sql

--- DBMS/Library_management/A_2_3-alterDefaultValuesOfIssueAndDueDateInBooks_Issue.sql
+++ DBMS/Library_management/A_2_3-alterDefaultValuesOfIssueAndDueDateInBooks_Issue.sql
@@ -1,3 +1,6 @@
+--Alter definitions of LIS tables to provide the default constraints in issue date and due date
+
+
 USE Library;
 ALTER TABLE `Books_issue` MODIFY COLUMN `issueDate` TIMESTAMP NOT NULL DEFAULT NOW();
 DELIMITER ;;
