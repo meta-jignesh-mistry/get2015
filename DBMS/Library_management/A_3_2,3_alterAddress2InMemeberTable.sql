700361c831ad43d83ac3b3a9d96e4e93a3d3d5f0
meta-jignesh-mistry <jignesh.mistry@metacube.com>
2015-09-10 11:11:40 +0530

    Update A_3_2,3_alterAddress2InMemeberTable.sql

--- DBMS/Library_management/A_3_2,3_alterAddress2InMemeberTable.sql
+++ DBMS/Library_management/A_3_2,3_alterAddress2InMemeberTable.sql
@@ -1,3 +1,8 @@
+--to Change value of addressLine2 column of Members(Users) table with “Jaipur” and 
+--to Change value of addressLine1 column of Members(Users)  table with value “EPIP, Sitapura” for the members belonging to category “F”.
+
+
+
 USE Library;
 UPDATE Users SET addressLine2 = 'Jaipur';
 select* from Users;
