b7f6fbd8f575532c97930bf8b7d7fb7706083a19
meta-jignesh-mistry <jignesh.mistry@metacube.com>
2015-09-11 18:39:48 +0530

    Update ZipCode.sql

--- DBMS/DBMS -2/zipCode/ZipCode.sql
+++ DBMS/DBMS -2/zipCode/ZipCode.sql
@@ -1,50 +1,56 @@
--- Creating Database zipcodeInfo
-CREATE DATABASE zipcode;
-USE zipcode;
 
--- creating table Zipcode
- CREATE TABLE zipcodeNumber
-(
-	zipcode int PRIMARY KEY AUTO_INCREMENT
-);
--- Creating table cityInfo
-CREATE TABLE cityInfo
-(
-	-- To store zip code and name
-	zipcode int PRIMARY KEY AUTO_INCREMENT,
-	cityName varchar(20),
-	FOREIGN KEY (zipcode) REFERENCES zipcodeNumber(zipcode) 
-);
--- Creating table stateInfo
-CREATE TABLE stateInfo
-(
-	-- to store zipcode and stateName
-	zipcode int PRIMARY KEY AUTO_INCREMENT,
+CREATE DATABASE getzip;
+Use getzip;
 
-	stateName varchar(20),
-	FOREIGN KEY (zipcode) REFERENCES zipcodeNumber(zipcode) 
+CREATE TABLE state(
+state_id INT AUTO_INCREMENT PRIMARY KEY,
+state_name VARCHAR(100)
 );
- 
- -- Inserting values in zipcodeNumber table
- INSERT INTO zipcodeNumber(zipcode) VALUES (800001);
- INSERT INTO zipcodeNumber(zipcode) VALUES (800002);
- INSERT INTO zipcodeNumber(zipcode) VALUES (800003);
- 
- -- Inserting values in cityInfo table
- INSERT INTO cityInfo(zipcode,cityName) VALUES (800001,'Aagra');
- INSERT INTO cityInfo(cityName) VALUES ('Jalore');
- INSERT INTO cityInfo(cityName) VALUES ('Ahemdabad');
- 
- -- Inserting values in stateInfo table
- INSERT INTO stateInfo(zipcode,stateName) VALUES (800001,'MP');
- INSERT INTO stateInfo(stateName) VALUES ('rajasthan');
- INSERT INTO stateInfo(stateName) VALUES ('Gujarat');
- 
- 
- -- Query to find the zip code city name and stateName
- SELECT z.zipcode,cityName,stateName from zipcodeNumber z, cityInfo c, stateInfo s where ((z. zipcode=c.zipcode)&&(z.zipcode=s.zipcode)) ORDER BY stateName asc , cityName asc;
 
- 
- 
- 
- 
+CREATE TABLE city(
+city_id INT AUTO_INCREMENT PRIMARY KEY,
+city_name VARCHAR(100),
+ state_id INT,
+ FOREIGN KEY (state_id)  REFERENCES state (state_id) on update cascade on delete cascade  
+);
+
+CREATE TABLE zipcode(
+   city_id INT,
+   zip_code VARCHAR(100),
+   FOREIGN KEY (city_id)  REFERENCES city (city_id) on update cascade on delete cascade
+);
+
+
+
+INSERT INTO state(state_name)VALUES ('Rajasthan');
+
+INSERT INTO state(state_name)VALUES ('Punjab');
+
+INSERT INTO state(state_name)VALUES ('Bihar');
+
+INSERT INTO city(city_name,state_id)VALUES ('Jaipur',1);
+
+INSERT INTO city(city_name,state_id)VALUES ('Ajmer',1);
+
+INSERT INTO city(city_name,state_id)VALUES ('Amritsar',2);
+
+INSERT INTO city(city_name,state_id)VALUES ('Chandighar',2);
+
+INSERT INTO city(city_name,state_id)VALUES ('Gaya',3);
+
+INSERT INTO city(city_name,state_id)VALUES ('Patna',3);
+INSERT INTO zipcode(city_id,zip_code)VALUES (1,'102007');
+
+INSERT INTO zipcode(city_id,zip_code)VALUES (2,'202007');
+
+INSERT INTO zipcode(city_id,zip_code)VALUES (3,'302007');
+INSERT INTO zipcode(city_id,zip_code)VALUES (4,'402007');
+
+INSERT INTO zipcode(city_id,zip_code)VALUES (5,'502007');
+
+
+INSERT INTO zipcode(city_id,zip_code)VALUES (6,'602567');
+
+/* write a SQL query for that returns a Resultset containing Zip Code, City Names and States ordered by State Name and City Name.*/
+select z.zip_code,c.city_name,s.state_name from zipcode z, city c,state s 
+where z.city_id=c.city_id and c.state_id = s.state_id;
