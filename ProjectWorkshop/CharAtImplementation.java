9c1f3443846694b7f6ee06e86496adabe0be96a3
Jignesh <jignesh.mistry@metacube.com>
2015-12-22 18:34:50 +0530

    CharAt and Throws Implementation

---
+++ ProjectWorkshop/CharAtImplementation.java
@@ -0,0 +1,27 @@
+package CharAt;
+
+public class CharAtImplementation{
+
+	private final char value[];
+
+	public CharAtImplementation() {
+		this.value = new char[0];
+	}
+
+	public CharAtImplementation original) {
+		this.value = original.value;
+	}
+
+	public char charAt(int index) {
+		if ((index < 0) || (index >= value.length)) {
+			throw new StringIndexOutOfBoundsException(index);
+		}
+		return value[index];
+	}
+
+	public static void main(String[] args) {
+		String str = "hello";
+		System.out.println(str.charAt(1));
+	}
+
+}
---
+++ ProjectWorkshop/ThrowAndThrows/Example.java
@@ -0,0 +1,22 @@
+package ThrowAndThrows;
+
+class Example
+{
+ static void check() throws ArithmeticException
+ {
+  System.out.println("Inside check function");
+  throw new ArithmeticException("Custom Exception");
+ }
+
+ public static void main(String args[])
+ {
+  try
+  {
+   check();
+  }
+  catch(ArithmeticException e)
+  {
+   System.out.println("caught " + e);
+  }
+ }
+}
